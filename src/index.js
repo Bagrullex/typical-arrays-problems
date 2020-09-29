c6984d627e50fbe265da38272004ca683e261817
Barys Chorny <bjegarrah@gmail.com>
2020-09-30 00:43:56 +0300

    fix: change functions

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array) {
+        return 0;
+    }
+    if (array.length === 0) {
+        return 0;
+    }
+
+    let len = array.length;
+    let min = Infinity;
+
+    while (len--) {
+        if (array[len] < min) {
+            min = array[len]
+        }
+    }
+    return min;
 }
+  
 
 exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
-
-const max = (coll) => {
-    if (coll.length === 0) {
-      return null;
+    if (!array) {
+        return 0;
     }
-  
-    let [max, ...rest] = coll;
+    if (array.length === 0) {
+        return 0;
+    }
+
+    let [max, ...rest] = array;
     for (const value of rest) {
       if (value > max) {
         max = value;
@@ -24,19 +35,26 @@ const max = (coll) => {
     }
   
     return max;
-  };
+}
 
-const avg = (coll) => {
-  const count = coll.length;
+exports.avg = function avg (array) {
+    if (!array) {
+        return 0;
+    }
+    if (array.length === 0) {
+        return 0;
+    }
+    
+    const count = array.length;
 
-  if (count === 0) {
-    return null;
-  }
-
-  let sum = 0;
-  for (const item of coll) {
-    sum += item;
-  }
-
-  return sum / count;
-};
+    if (count === 0) {
+      return 0;
+    }
+  
+    let sum = 0;
+    for (const item of array) {
+      sum += item;
+    }
+  
+    return sum / count;
+}
